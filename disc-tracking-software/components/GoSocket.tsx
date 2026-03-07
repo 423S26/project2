45461e4f438183cdbb4891f19c857bde8aeb845b
Jaden Hampton <techiesbestfriend@gmail.com>
2026-03-06 22:06:42 -0700

    Bug fixes, finalllyyy

--- disc-tracking-software/components/GoSocket.tsx
+++ disc-tracking-software/components/GoSocket.tsx
@@ -24,16 +24,6 @@ export default function LiveTracker() {
 
 	return ( // THIS IS TEMPORARY BUT TO GIVE YOU AN IDEA OF HOW TO CALL BACKEND FOR LIVE STATS, I JUST NEEDED SOMETHING TO FILL THIS SPACE, IN ACTUALITY WE WILL BE BUILDING THIS OUT TO BE IMPORTED AS A TYPICAL COMPONENT WITH YOUR TRACKING END...
 		<div className="p-4 bg-slate-900 text-white rounded-lg">
-			<h2 className="text-xl font-bold">Live Disc Location</h2>
-			{lastPing ? (
-				<p>Lat: {lastPing.latitude} | Lon: {lastPing.longitude}</p>
-			) : (
-				<p>Waiting for throw...</p>
-			)}
 		</div>
-		<div className={`p-4 rounded ${statusColors[status]}`}>
-    			<h3>Disc Status: {status}</h3>
-    			{status === "IN_FLIGHT" && <p>Current Spin: {liveRpm} RPM</p>}
-  		</div>
 	);
 }
